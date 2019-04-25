51a9903ca2f9273876974c4dc44cff0cdaa35bdd
Mic Kajzer <mickaj@mkajzer.pl>
2019-04-25 21:03:49 +0200

    ITaskManager added

---
+++ DataModel/Abstractions/ITaskManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Abstractions
+{
+    public interface ITaskManager
+    {
+        void AddTask(ITaskModel task);
+        void RemoveTask(ITaskModel task);
+        IEnumerable<ITaskModel> GetTasks();
+        ITaskModel GetTask(int index);
+    }
+}
--- DataModel/TaskManager.cs
+++ DataModel/TaskManager.cs
@@ -1,44 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DataModel.Abstractions;
 
 namespace DataModel
 {
-    public class TaskManager
+    public class TaskManager : ITaskManager
     {
         private const string TaskAlreadyPresentExceptionMessage = "Task already in collection. Cannot add twice!";
         private const string TaskDoesntBelongToCollectionExceptionMessage = "Cannot removed the task as it doesn't belong to collection.";
         private const string TaskWithGivenIndexDoesNotExistsExceptionMessage = "Task with given index doesn't exist in collection";
 
-        private List<TaskModel> _tasks = new List<TaskModel>();
+        private List<ITaskModel> _tasks = new List<ITaskModel>();
 
         public TaskManager()
         {
         }
 
-        public TaskManager(IEnumerable<TaskModel> source)
+        public TaskManager(IEnumerable<ITaskModel> source)
         {
-            _tasks = new List<TaskModel>(source);
+            _tasks = new List<ITaskModel>(source);
         }
 
-        public void AddTask(TaskModel task)
+        public void AddTask(ITaskModel task)
         {
             if (_tasks.Contains(task)) { throw new ArgumentException(TaskAlreadyPresentExceptionMessage); }
             else { _tasks.Add(task); }
         }
 
-        public void RemoveTask(TaskModel task)
+        public void RemoveTask(ITaskModel task)
         {
             if (_tasks.Contains(task)) { _tasks.Remove(task); }
             else { throw new ArgumentException(TaskDoesntBelongToCollectionExceptionMessage); }
         }
 
-        public IEnumerable<TaskModel> GetTasks()
+        public IEnumerable<ITaskModel> GetTasks()
         {
-            return new List<TaskModel>(_tasks) as IEnumerable<TaskModel>;
+            return new List<ITaskModel>(_tasks) as IEnumerable<ITaskModel>;
         }
 
-        public TaskModel GetTask(int index)
+        public ITaskModel GetTask(int index)
         {
             if(_tasks.Count > index) {return _tasks[index];}
             else {throw new ArgumentException(TaskWithGivenIndexDoesNotExistsExceptionMessage);}
