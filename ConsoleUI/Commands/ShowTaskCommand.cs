fe50dbae5463b370553c1441e5c575e31fbb1bda
Mic Kajzer <mickaj@mkajzer.pl>
2019-05-10 21:38:18 +0200

    ShowTask command scaffolded

---
+++ ConsoleUI/Commands/ShowTaskCommand.cs
@@ -0,0 +1,26 @@
+﻿using DataModel.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI.Commands
+{
+    public class ShowTaskCommand : UiCommand, IUiCommand
+    {
+        private readonly ITaskManager _taskManager;
+
+        public string Name { get; } = "show";
+        public string HelpMessage { get; } = "Display all properties of a task sepcified by its index.\n* Usage: type command and hit press enter, you will be prompted for task's index.";
+
+        public ShowTaskCommand(IConsole console, ITaskManager taskManager)
+            :base(console)
+        {
+            _taskManager = taskManager;
+        }
+
+        public void Invoke()
+        {
+            _console.WriteLine("task details");
+        }
+    }
+}
