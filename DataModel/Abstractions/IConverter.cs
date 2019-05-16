177e8e2dd2aec5ab01648ecf667cb449ea62e164
Mic Kajzer <mickaj@mkajzer.pl>
2019-05-16 19:39:20 +0200

    DataModel/CsvConverter added

---
+++ DataModel/Abstractions/IConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel.Abstractions
+{
+    public interface IConverter
+    {
+        string[] ToStringArray(IEnumerable<ITaskModel> source);
+    }
+}
---
+++ DataModel/CsvConverter.cs
@@ -0,0 +1,34 @@
+﻿using DataModel.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel
+{
+    public class CsvConverter : IConverter
+    {
+        public string[] ToStringArray(IEnumerable<ITaskModel> source)
+        {
+            List<string> result = new List<string>();
+            foreach(ITaskModel task in source)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendJoin(',', BuildPropertiesStrings(task));
+                result.Add(sb.ToString());
+            }
+            return result.ToArray();
+        }
+
+        private string[] BuildPropertiesStrings(ITaskModel task)
+        {
+           return new string[] 
+           {
+               $"\u0022{task.Name}\u0022",
+               $"\u0022{task.Description}\u0022",
+               $"\u0022{task.StartDate.ToString()}\u0022",
+               $"\u0022{task.EndDate.ToString()}\u0022",
+               $"\u0022{task.Important.ToString()}\u0022"
+           };
+        }
+    }
+}
